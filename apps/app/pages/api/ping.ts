81c68a21ff8145b5b48d477f7ec70b02f132bd76
TaylorFacen <taylor.facen@gmail.com>
2022-12-15 15:21:11 -0500

    Include headers

--- apps/app/pages/api/ping.ts
+++ apps/app/pages/api/ping.ts
@@ -3,7 +3,7 @@ import { withAxiom, AxiomRequest } from 'next-axiom'
 export const config = { runtime: 'experimental-edge' };
 
 async function handler(req: AxiomRequest) {
-  req.log.info("Pong");
+  req.log.info("Pong", { headers: req.headers });
   return new Response("Pong")
 }
 
