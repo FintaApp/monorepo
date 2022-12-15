002c0b98a9cddc69da4332a543af3b3aacd22fcb
TaylorFacen <taylor.facen@gmail.com>
2022-12-15 17:24:55 -0500

    Experiment 3

--- apps/app/pages/api/ping.ts
+++ apps/app/pages/api/ping.ts
@@ -4,7 +4,7 @@ export const config = { runtime: 'experimental-edge'};
 
 const handler = async (req: AxiomRequest ) => {
   const route = req.nextUrl.pathname;
-  req.log.info(`${route} request started`, { headers: req.headers, route: req.nextUrl, cookies: req.cookies, url: req.url });  
+  req.log.info(`${route} request started`, { id: req.headers.get('x-vercel-id'), testHost: req.headers.get('host'), keys: req.headers.keys });  
   req.log.info("Pong")
   req.log.info(`${route} request finished`, { status: 200, message: "Pong" });
   return new Response("Pong")
