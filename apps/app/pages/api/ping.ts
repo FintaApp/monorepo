936b74db62567ac53de9eb7256cbba6fc970a726
TaylorFacen <taylor.facen@gmail.com>
2022-12-15 17:15:23 -0500

    Experiment 2

--- apps/app/pages/api/ping.ts
+++ apps/app/pages/api/ping.ts
@@ -4,7 +4,7 @@ export const config = { runtime: 'experimental-edge'};
 
 const handler = async (req: AxiomRequest ) => {
   const route = req.nextUrl.pathname;
-  req.log.info(`${route} request started`, { headers: req.headers, route: req.nextUrl, req });  
+  req.log.info(`${route} request started`, { headers: req.headers, route: req.nextUrl, cookies: req.cookies, url: req.url });  
   req.log.info("Pong")
   req.log.info(`${route} request finished`, { status: 200, message: "Pong" });
   return new Response("Pong")
