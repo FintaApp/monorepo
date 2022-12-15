6c92effaf38e1255ef9921aa86ac10cc44f38151
TaylorFacen <taylor.facen@gmail.com>
2022-12-15 15:26:58 -0500

    Switched log statement

--- apps/app/pages/api/ping.ts
+++ apps/app/pages/api/ping.ts
@@ -3,7 +3,8 @@ import { withAxiom, AxiomRequest } from 'next-axiom'
 export const config = { runtime: 'experimental-edge' };
 
 async function handler(req: AxiomRequest) {
-  req.log.info("Pong", { headers: req.headers });
+  //req.log.info("Pong", { headers: req.headers });
+  req.log.info("Headers: " + JSON.stringify(req.headers), { headers: JSON.parse(JSON.stringify(req.headers))})
   return new Response("Pong")
 }
 
