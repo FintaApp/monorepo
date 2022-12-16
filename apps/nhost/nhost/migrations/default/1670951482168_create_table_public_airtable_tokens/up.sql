bb2d160ff8532f813315639472096c10563e647f
TaylorFacen <taylor.facen@gmail.com>
2022-12-15 19:59:36 -0500

    Don't recreate function

--- apps/nhost/nhost/migrations/default/1670951482168_create_table_public_airtable_tokens/up.sql
+++ apps/nhost/nhost/migrations/default/1670951482168_create_table_public_airtable_tokens/up.sql
@@ -1,14 +1,4 @@
 CREATE TABLE "public"."airtable_tokens" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "user_id" uuid NOT NULL, "access_token" text NOT NULL, "refresh_token" text NOT NULL, "token_type" text NOT NULL, "scope" text NOT NULL, "refreshed_at" timestamptz NOT NULL DEFAULT now(), "access_token_expires_at" timestamptz NOT NULL, "refresh_token_expires_at" timestamptz NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON UPDATE restrict ON DELETE cascade);COMMENT ON TABLE "public"."airtable_tokens" IS E'Used to house Airtable access and refresh tokens for users';
-CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
-RETURNS TRIGGER AS $$
-DECLARE
-  _new record;
-BEGIN
-  _new := NEW;
-  _new."updated_at" = NOW();
-  RETURN _new;
-END;
-$$ LANGUAGE plpgsql;
 CREATE TRIGGER "set_public_airtable_tokens_updated_at"
 BEFORE UPDATE ON "public"."airtable_tokens"
 FOR EACH ROW
