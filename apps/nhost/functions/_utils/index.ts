0c86478858ba6e9d039a5c15a7935d6b30fe1408
TaylorFacen <taylor.facen@gmail.com>
2022-12-15 20:11:17 -0500

    WTAF

--- apps/nhost/functions/_utils/index.ts
+++
@@ -1,5 +0,0 @@
-export * as analytics from "./segment";
-export { graphql } from "./graphql";
-export { Logger } from "./logger";
-export * as stripe from "./stripe"; 
-export * as stripeEventFunctions from "./stripeEvents"; 
