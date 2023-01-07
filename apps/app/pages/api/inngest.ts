150a9b84d17a3fda07f2beee375206953d8c51b5
TaylorFacen <taylor.facen@gmail.com>
2023-01-07 03:41:37 -0500

    Serve inngest function

--- apps/app/pages/api/inngest.ts
+++ apps/app/pages/api/inngest.ts
@@ -1,5 +1,6 @@
 import { serve } from "inngest/next";
 import { setupPlaidItem } from "~/lib/inngest/setupPlaidItem";
 import { syncDestination } from "~/lib/inngest/syncDestination";
+import { sendPeriodicUpdateEmail } from "~/lib/inngest/sendPeriodicUpdateEmail";
 
-export default serve("Finta Jobs", [ setupPlaidItem, syncDestination ])
+export default serve("Finta Jobs", [ setupPlaidItem, syncDestination, sendPeriodicUpdateEmail ])
